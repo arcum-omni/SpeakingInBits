9aceb0d605f567c1ac9f0b56190ba85dcd8b95c7
arcum-omni <travis.l.eiler@gmail.com>
2020-04-13 10:03:23 -0700

    Create student and instructor roles on startup

--- Models/IdentityHelper.cs
+++ Models/IdentityHelper.cs
@@ -1,13 +1,19 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace SpeakingInBits.Models
 {
     public static class IdentityHelper
     {
+        // Role Names
+        public const string InstructorRole = "Instructor";
+        public const string StudentRole = "Student";
+
         public static void SetIdentityOptions(IdentityOptions options)
         {
             // Set Sign In Options
@@ -24,6 +30,22 @@ namespace SpeakingInBits.Models
             // Set Lockout Options
             options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
             options.Lockout.MaxFailedAccessAttempts = 5;
+        }
+
+        public static async Task CreateRoles(IServiceProvider provider, params string[] roles)
+        {
+            RoleManager<IdentityRole> roleMgr = provider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            // create role if it does not exist
+            foreach (string role in roles)
+            {
+                bool doesRoleExist = await roleMgr.RoleExistsAsync(role);
+
+                if (!doesRoleExist)
+                {
+                    await roleMgr.CreateAsync(new IdentityRole(role));
+                }
+            }
         }
     }
 }
