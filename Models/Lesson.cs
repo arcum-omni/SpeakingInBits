afb7d588bd3886297755fb7077f47b979810a665
arcum-omni <travis.l.eiler@gmail.com>
2020-04-15 09:56:14 -0700

    Created video lesson model

---
+++ Models/Lesson.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpeakingInBits.Models
+{
+    /// <summary>
+    /// A generic course lesson
+    /// </summary>
+    public abstract class Lesson
+    {
+        [Key]
+        public int LessonID { get; set; }
+
+        [Required]
+        public string LessonTitle { get; set; }
+
+        public string LessonDescription { get; set; }
+    }
+
+    /// <summary>
+    /// Video Lecture Lesson
+    /// </summary>
+    public class VideoLesson : Lesson
+    {
+        /// <summary>
+        /// Embed Code for Video,
+        /// IE YouTube, Vimeo, etc.
+        /// </summary>
+        public string EmbedCode { get; set; }
+    }
+}
